2fd2002cbeeb19c560f280d0eb7138526a1f10f0
Letícia Farage <162373412+lefarage@users.noreply.github.com>
2024-10-30 20:42:02 +0000

    span no js

--- graficos/inforcacoesGlobais.js
+++ graficos/inforcacoesGlobais.js
@@ -6,7 +6,7 @@ async function vizualizarInformacoesGlobais() {
     console.log(dados)
     const paragrafo = document.createElement('p')
         paragrafo.classList.add('graficos-container__texto')
-        paragrafo.innerHTML = `As pessoas têm um nítido interesse por esportes, pois mais da metade da população mundial (${dados.total_pessoas_que_assistem_esportes} das ${dados.total_pessoas_mundo}) acompanha esportes pelas telas. No entanto, esse interesse permanece naufragado no sofá, já que ${dados.total_pessoas_que_praticam_esportes_regularmente} de fato praticam algum esporte. Somado a isso, a quantidade de tempo despendido na prática é ainda mais baixa, de apenas ${dados.tempo_medio_semana_praticando_esportes} horas por semana.`
+        paragrafo.innerHTML = `As pessoas têm um nítido interesse por esportes, pois mais da metade da população mundial ( <span>${dados.total_pessoas_que_assistem_esportes}</span> das <span>${dados.total_pessoas_mundo}</span>) acompanha esportes pelas telas. No entanto, esse interesse permanece naufragado no sofá, já que <span>${dados.total_pessoas_que_praticam_esportes_regularmente}</span> de fato praticam algum esporte. Somado a isso, a quantidade de tempo despendido na prática é ainda mais baixa, de apenas <span>${dados.tempo_medio_semana_praticando_esportes}</span> horas por semana.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
 }
